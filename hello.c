205a2c7c39a85d7d1a53dc30ce7075091f14d0d5
yemuyou <bli24@huskers.unl.edu>
2021-08-31 03:32:55 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Bingquan Li
+ *         bli24@cse.unl.edu
+ * Date: 2021/08/30
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+  printf("Bingquan Li\n");
+
+  return 0;
+}
